6ad0254575d6516feaaa1b8b83bf9b1cc429c2ed
backmrpun-hash <backmrpun@gmail.com>
2026-05-18 01:33:34 +0700

    Update ij.ps1

--- ij.ps1
+++ ij.ps1
@@ -50,7 +50,7 @@ try {
 }
 
 # --- CONFIGURATION ---
-$GithubDllUrl = "https://raw.githubusercontent.com/backmrpun-hash/PS/refs/heads/main/d.dll"
+$GithubDllUrl = "https://raw.githubusercontent.com/backmrpun-hash/PS/refs/heads/main/Reshade.dll"
 $LocalDllPath = "$env:TEMP\version.dll"
 # ---------------------
 
